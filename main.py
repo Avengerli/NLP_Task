3f887dafec6827a5037a18f0a37b8f4bbe18f20c
Avengerli <59286634+Avengerli@users.noreply.github.com>
2023-01-14 21:29:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,13 @@ if __name__ == '__main__':
     # CNN_Train.main(1, 100)
 
     # AlexNet模型，训练100epoch
+    # CNN_Train中103、108行参数设置为227，即
+    # transforms.RandomResizedCrop(227),
+    # transforms.CenterCrop(227),
     CNN_Train.main(2, 100)
 
     # ResNet模型，训练100epoch
+    # CNN_Train中103、108行参数设置为224，即
+    # transforms.RandomResizedCrop(224),
+    # transforms.CenterCrop(224),
     # CNN_Train.main(3,100)
-
